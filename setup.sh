6961013d96b519f42365fa1bf7e8517d48a47fb9
dannadori <dannadori>
2020-04-11 05:31:38 +0000

    add conf

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo '- download the base model and put into BASE_MODEL. base model is here.'
+echo '  https://drive.google.com/file/d/1pEs6dFg88CH8LNNUSg5-ebcqu9qk5C-h/view?usp=sharing'
+echo '- see more info,'
+echo '  https://github.com/dannadori/fast-semantic-segmentation/blob/master/docs/model_zoo.md'
+
+
---
+++ setup_my_project.sh
@@ -0,0 +1,15 @@
+python3 SCRIPT/generate_1st_stage_conf.py  \
+	--num_class 2 \
+	--crop_height 601 \
+	--crop_width 601 \
+	--eval_width 601 \
+	--eval_height 601 \
+	--train_input_path aaa \
+	--eval_input_path aa \
+	--train_num_examples 400 \
+	--eval_num_examples 400 \
+	--filter_scale 0.5 \
+	--fine_tune_checkpoint sddd
+
+
+
