624861d5250ff0281d665130b009afe95406e6b5
dannadori <dannadori>
2020-04-11 06:08:03 +0000

    add conf

--- SCRIPT/generate_conf_and_cmds.py
+++ SCRIPT/generate_conf_and_cmds.py
@@ -18,20 +18,25 @@ parser.add_argument('--eval_input_path',    default=300,     required=True,  typ
 
 ## 2nd stage
 parser.add_argument('--filter_scale',         default=0,     required=True,  type=float)
-parser.add_argument('--fine_tune_checkpoint', default=None,  required=True,  type=str)
+#parser.add_argument('--fine_tune_checkpoint', default=None,  required=True,  type=str)
 
 if __name__ == '__main__':
     args = parser.parse_args()
     context = vars(args)
+    context['fine_tune_checkpoint'] = 'STAGE1/icnet_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}/comp/pruned_model.ckpt'
+
 
     DIR_FOR_1ST_STAGE = os.path.join('TRAIN_CONF','1st_stage')
     DIR_FOR_2ND_STAGE = os.path.join('TRAIN_CONF','2nd_stage')
+    DIR_FOR_CMD       = 'EXEC_CMDS'
 
     CONF_NAME_FOR_1ST_STAGE = f'icnet_1st_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}.conf'
     CONF_NAME_FOR_2ND_STAGE = f'icnet_2nd_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}.conf'
+    CMD_NAME                = f'cmds_for_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}.txt'
 
     CONF_PATH_FOR_1ST_STAGE = os.path.join(DIR_FOR_1ST_STAGE, CONF_NAME_FOR_1ST_STAGE)
     CONF_PATH_FOR_2ND_STAGE = os.path.join(DIR_FOR_2ND_STAGE, CONF_NAME_FOR_2ND_STAGE)
+    CMD_PATH                = os.path.join(DIR_FOR_CMD, CMD_NAME)
     
     print(CONF_PATH_FOR_1ST_STAGE)
     print(CONF_PATH_FOR_2ND_STAGE)
@@ -40,7 +45,7 @@ if __name__ == '__main__':
     with open(os.path.join(DIR_FOR_1ST_STAGE, 'template.conf')) as f:
         temp = f.read()
     template = string.Template(temp)
-    conf = template.substitute(context)
+    conf     = template.substitute(context)
     with open(CONF_PATH_FOR_1ST_STAGE, 'w') as f:
         f.write(conf)
 
@@ -48,9 +53,25 @@ if __name__ == '__main__':
     with open(os.path.join(DIR_FOR_2ND_STAGE, 'template.conf')) as f:
         temp = f.read()
     template = string.Template(temp)
-    conf = template.substitute(context)
+    conf     = template.substitute(context)
     with open(CONF_PATH_FOR_2ND_STAGE, 'w') as f:
         f.write(conf)
 
+    # for cmds
+    with open(os.path.join(DIR_FOR_CMD,'template.txt')) as f:
+        temp = f.read()
+    template = string.Template(temp)
+    context={
+        'stage1_config': CONF_PATH_FOR_1ST_STAGE,
+        'train_tag'    : f'icnet_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}',
+        'shape'        : f'-1,{args.crop_width}{args.crop_height},3',
+        'num_steps'    : f'{args.num_steps}',
+        'filter_scale' : f'{args.filter_scale}',
+        'stage2_config': CONF_PATH_FOR_2ND_STAGE,
+    }
+    cmd      = template.substitute(context)    
+    with open(CMD_PATH, 'w') as f:
+        f.write(cmd)
+                     
 
-
+        
--- setup_my_project.sh
+++ setup_my_project.sh
@@ -1,15 +1,14 @@
-python3 SCRIPT/generate_1st_stage_conf.py  \
+python3 SCRIPT/generate_conf_and_cmds.py  \
 	--num_class 2 \
 	--crop_height 601 \
 	--crop_width 601 \
 	--eval_width 601 \
 	--eval_height 601 \
-	--train_input_path aaa \
-	--eval_input_path aa \
 	--train_num_examples 400 \
 	--eval_num_examples 400 \
 	--filter_scale 0.5 \
-	--fine_tune_checkpoint sddd
+	--train_input_path aaa \
+	--eval_input_path aa \
 
 
 
