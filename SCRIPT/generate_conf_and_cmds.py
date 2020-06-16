36ef8b4f94dedf714170df739f3bda7b4d9fd1ae
wataru okada <wataru.okada@flect.co.jp>
2020-06-16 10:21:12 +0900

    update

--- SCRIPT/generate_conf_and_cmds.py
+++ SCRIPT/generate_conf_and_cmds.py
@@ -7,12 +7,14 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--num_classes',        default=2,       required=True,  type=int)
 parser.add_argument('--batch_size',         default=32,      required=False, type=int)
 parser.add_argument('--num_steps',          default=12000,   required=False, type=int)
-parser.add_argument('--crop_height',        default=300,     required=True,  type=int)
-parser.add_argument('--crop_width',         default=300,     required=True,  type=int)
+parser.add_argument('--height',             default=300,     required=True,  type=int)
+parser.add_argument('--width',              default=300,     required=True,  type=int)
+#parser.add_argument('--crop_height',        default=300,     required=True,  type=int)
+#parser.add_argument('--crop_width',         default=300,     required=True,  type=int)
 parser.add_argument('--train_num_examples', default=300,     required=True,  type=int)
 parser.add_argument('--eval_num_examples',  default=300,     required=True,  type=int)
-parser.add_argument('--eval_height',        default=300,     required=True,  type=int)
-parser.add_argument('--eval_width',         default=300,     required=True,  type=int)
+#parser.add_argument('--eval_height',        default=300,     required=True,  type=int)
+#parser.add_argument('--eval_width',         default=300,     required=True,  type=int)
 parser.add_argument('--train_input_path',   default=300,     required=True,  type=str)
 parser.add_argument('--eval_input_path',    default=300,     required=True,  type=str)
 
@@ -23,16 +25,21 @@ parser.add_argument('--filter_scale',         default=0,     required=True,  typ
 if __name__ == '__main__':
     args = parser.parse_args()
     context = vars(args)
-    context['fine_tune_checkpoint'] = f'STAGE1/icnet_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}/comp/pruned_model.ckpt'
+    context['fine_tune_checkpoint'] = f'STAGE1/icnet_{args.width:04d}x{args.height:04d}_{args.filter_scale:02.2f}/comp/pruned_model.ckpt'
+    context['crop_height'] = args.height + 1
+    context['crop_width']  = args.width  + 1
+    context['eval_height'] = args.height + 1
+    context['eval_width']  = args.width  + 1
+
 
 
     DIR_FOR_1ST_STAGE = os.path.join('TRAIN_CONF','1st_stage')
     DIR_FOR_2ND_STAGE = os.path.join('TRAIN_CONF','2nd_stage')
     DIR_FOR_CMD       = 'EXEC_CMDS'
 
-    CONF_NAME_FOR_1ST_STAGE = f'icnet_1st_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}.conf'
-    CONF_NAME_FOR_2ND_STAGE = f'icnet_2nd_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}.conf'
-    CMD_NAME                = f'cmds_for_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}.txt'
+    CONF_NAME_FOR_1ST_STAGE = f'icnet_1st_{args.width:04d}x{args.height:04d}_{args.filter_scale:02.2f}.conf'
+    CONF_NAME_FOR_2ND_STAGE = f'icnet_2nd_{args.width:04d}x{args.height:04d}_{args.filter_scale:02.2f}.conf'
+    CMD_NAME                = f'cmds_for_{args.width:04d}x{args.height:04d}_{args.filter_scale:02.2f}.txt'
 
     CONF_PATH_FOR_1ST_STAGE = os.path.join(DIR_FOR_1ST_STAGE, CONF_NAME_FOR_1ST_STAGE)
     CONF_PATH_FOR_2ND_STAGE = os.path.join(DIR_FOR_2ND_STAGE, CONF_NAME_FOR_2ND_STAGE)
@@ -64,8 +71,8 @@ if __name__ == '__main__':
     template = string.Template(temp)
     context={
         'stage1_config': CONF_PATH_FOR_1ST_STAGE,
-        'train_tag'    : f'icnet_{args.crop_width:04d}x{args.crop_height:04d}_{args.filter_scale:02.2f}',
-        'shape'        : f'-1,{args.crop_width},{args.crop_height},3',
+        'train_tag'    : f'icnet_{args.width:04d}x{args.height:04d}_{args.filter_scale:02.2f}',
+        'shape'        : f'-1,{args.width},{args.height},3',
         'num_steps'    : f'{args.num_steps}',
         'filter_scale' : f'{args.filter_scale}',
         'stage2_config': CONF_PATH_FOR_2ND_STAGE,
--- setup_my_project.sh
+++ setup_my_project.sh
@@ -1,14 +1,21 @@
 python3 SCRIPT/generate_conf_and_cmds.py  \
 	--num_class 2 \
-	--crop_height 601 \
-	--crop_width 601 \
-	--eval_width 601 \
-	--eval_height 601 \
-	--train_num_examples 400 \
-	--eval_num_examples 400 \
-	--filter_scale 0.5 \
-	--train_input_path aaa \
-	--eval_input_path aa \
+	--num_steps 12000 \
+	--height 300 \
+	--width 300 \
+	--train_num_examples 24340 \
+	--eval_num_examples 2870 \
+	--filter_scale 0.1 \
+	--train_input_path TFRECORDS/train_2434.record \
+	--eval_input_path  TFRECORDS/test_287.record   \
+	
+#	--train_input_path ../FLECT-BARCODE/dataset/DATA004_TFRECORDS_SS/002_icnet2_300x300/train_2434.record \
+#	--eval_input_path  ../FLECT-BARCODE/dataset/DATA004_TFRECORDS_SS/002_icnet2_300x300/test_287.record   \
 
 
+# ※ example数は10度ずつ10回転させているので10倍
+#	--train_num_examples 24340 
+#	--eval_num_examples 2870
 
+
+	
